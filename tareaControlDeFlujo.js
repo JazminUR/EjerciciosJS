4749938c1815e36a69c9344b31590d1714c555c1
JazminUR <90486546+JazminUR@users.noreply.github.com>
2022-01-14 20:53:52 -0600

    modificación de tarea de matrices y feliprueba del día viernes de la semana 5

--- tareaControlDeFlujo.js
+++ tareaControlDeFlujo.js
@@ -8,15 +8,18 @@ It should return true if it is business hours, and false if not.
 function businessHours(dayNumber, hourNumber)
 Obs: consider business hours 9am - 6pm. Use the 24-hour clock system (6pm = 18). */
 
-function businessHours(dayNumber, hourNumber) {
-    if (hourNumber >= 9 && hourNumber <= 18 && dayNumber > 0 && dayNumber < 6) {
-    alert (true);
-    } else {
-    alert (false);
+function businessHours(dayNumber,hourNumber) {
+    //var dayNumber=parseInt(prompt("Ingrese el dia con numero "))
+    var hourNumber=parseInt(prompt("Ingrese la hora en horario de 24 hrs"))
+    
+    if (hourNumber >= 9 && hourNumber <= 18 && dayNumber >= 0 && dayNumber <= 6){
+        window.alert(true);
+    }else  {
+        
+        window.alert(false)
+    }   
     }
-}
-businessHours(6,19);
-
+    //businessHours();
 
 /* Part 2
 Now, create the function getDayNumber.
@@ -26,68 +29,22 @@ janFirstDayNumber will be an int ranging from 0 to 6, representing the day of th
 Your function should then calculate and return the day of the week corresponding to the yearDayNumber passed.
 Hint: use the remainder operator (%), dividing your yearDayNumber by 7. */
 
-var getDayNumber = parseInt(prompt("Dame un día del 0 al 365"));
-var calculator = getDayNumber % 7
-switch (calculator){
-    case 1: 
-        console.log("Lunes")
-        break;
-    case 2:
-        console.log("Martes")
-        break;
-    case 3:
-        console.log("Miercoles")
-        break;
-    case 4:
-        console.log("Jueves")
-        break;
-    case 5:
-        console.log("Viernes")
-        break;
-    case 6:
-        console.log("Sabado")
-        break;
-    case 7:
-        console.log("Domingo")
-        break;
+function getDayNumber(janFirstDayNumber, yearDayNumber){
+    var res = yearDayNumber % 7; 
+    var final = (janFirstDayNumber + res)%7;
+    return final;
 }
+//getDayNumber();
+
+//alert("El dia de la semana en el que cayo fue el : " + getDayNumber(parseInt(prompt("Que dia cayo el 1ro de Enero?(0-6)")),parseInt(prompt("Que dia quieres conocer?(0-365)"))));
 
 /*Part 3
 Finally, build a function that, from a yearDayNumber (int, 0-365) and an hourNumber (int, 0-23), 
 returns true if it is business hours. */
-/* function businessHours(yearDayNumber, hourNumber) {
-    var getDayNumber = parseInt(prompt("Dame un día del 0 al 365"));
-    var calculator = getDayNumber % 7
-switch (calculator){
-    case 1: 
-        console.log("Lunes")
-        break;
-    case 2:
-        console.log("Martes")
-        break;
-    case 3:
-        console.log("Miercoles")
-        break;
-    case 4:
-        console.log("Jueves")
-        break;
-    case 5:
-        console.log("Viernes")
-        break;
-    case 6:
-        console.log("Sabado")
-        break;
-    case 7:
-        console.log("Domingo")
-        break;
+
+function workingHours() {
+    getDayNumber();
+    businessHours();
 }
-var result = calculator 
-}
-var hourNumber = parseInt(prompt("Dame una hora, considerando del 0 al 23."));
-    if (hourNumber >= 9 && hourNumber <= 18 && result > 0 && result < 6) {
-        alert (true);
-    } else {
-        alert (false);
-    }
-    businessHours(); 
-*/
+workingHours();
+alert("El dia de la semana en el que cayo fue el : " + getDayNumber(parseInt(prompt("Que dia cayo el 1ro de Enero?(0-6)")),parseInt(prompt("Que dia quieres conocer?(0-365)"))));
