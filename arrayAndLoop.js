41918af8171a4295b00df49569df6043cbe1dbaf
JazminUR <90486546+JazminUR@users.noreply.github.com>
2022-01-18 23:35:44 -0600

    terminé el ultimo ejercicio de la actividad

--- arrayAndLoop.js
+++ arrayAndLoop.js
@@ -1,21 +1,52 @@
-//Exercise #1 | PENDIENTE
-/*Part 1
-There are three people waiting for the bank. Their names, in order, are Sofia, David, and Juan. 
-Create an array that has these three names in order.
-Part 2
-Two more people get added to the back of the line - Sara and Augustin. The first person in line is called to 
-the teller. What does the queue look like?
-Part 3
-It turns out David was saving a spot for his friend Renata. She shows up and goes behind him in the line. 
-One more person (Elena) shows up and goes to the end of the line. What does the queue look like? */
+//Exercise #1 | OK
+// Part 1
+//There are three people waiting for the bank. Their names, in order, are Sofia, David, and Juan. 
+//Create an array that has these three names in order.
+var people = ["Sofia", "David", "Juan"];
+console.log(people);
 
+//Part 2
+//Two more people get added to the back of the line - Sara and Augustin. The first person in line is called to 
+//the teller. What does the queue look like?
+people.splice(0,1);
+console.log(people); //Sofia goes to the teller
+
+people.splice (0,2); //delete david and juan
+console.log(people)
+
+people.unshift("Augustin"); //add augustin
+console.log(people);
+
+people.unshift("Sara"); //add sara
+console.log(people);
+
+people.unshift("Juan"); //add juan
+console.log(people);
+
+people.unshift("David"); //add david
+console.log(people);
+
+//Part 3
+/*It turns out David was saving a spot for his friend Renata. She shows up and goes behind him in the line. 
+One more person (Elena) shows up and goes to the end of the line. What does the queue look like?*/
+people.unshift("Renata"); //add renata
+console.log(people);
+
+people.splice(1,1); //Remove david
+console.log(people);
+
+people.unshift("David"); //add renata
+console.log(people);
+
+people.push("Elena"); //add elena to the back
+console.log(people);
 
 // Exercise #2 | OK
 // Write a JavaScript program to construct the following pattern, using a nested for loop.
 function Pyramid(number, symbol) {
     for (let num = 1; num <= number; num++) {
         let pyramid = "".padStart(num,symbol); //El metodo padStart rellena una cadena para tener el alcance de longitud dado, en este caso en la función es 5
-        console.log(pyramid); 
+        //console.log(pyramid); 
     }
 }
 Pyramid(5, "*");
@@ -44,7 +75,7 @@ for (let i = 1; i <= 100; i++) {
 var iterator = 1;
 while (iterator <= 100){
     if (iterator % 2 !== 0){
-        console.log(iterator);
+        //console.log(iterator);
     }
     iterator = iterator + 1;
 }
